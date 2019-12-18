747177f869de3ca6739231bd8bad64923a9b1442
Dawid <dawidadamczykeit@gmail.com>
2019-12-18 16:34:56 +0100

    add models

--- django_engineering_project/settings.py
+++ django_engineering_project/settings.py
@@ -32,6 +32,7 @@ ALLOWED_HOSTS = []
 
 INSTALLED_APPS = [
     'cars',
+    'employees',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
--- employees/models.py
+++ employees/models.py
@@ -1,3 +1,10 @@
 from django.db import models
+from django.contrib.auth.models import User
+from cars.models import Car
 
-# Create your models here.
+class Employee(models.Model):
+    position = models.CharField(max_length=50)
+    age = models.PositiveSmallIntegerField()
+    sex = models.CharField(max_length=6)
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    car = models.ForeignKey(Car, null='true', on_delete=models.CASCADE)
