9dc799c68b68a2752559ec5399864a00d2686446
Dawid <dawidadamczykeit@gmail.com>
2019-12-18 13:32:26 +0100

    Empty apps

--- django_engineering_project/settings.py
+++ django_engineering_project/settings.py
@@ -75,8 +75,10 @@ WSGI_APPLICATION = 'django_engineering_project.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'db_engineering_project',
+        'USER': 'user',
+        'PASSWORD': 'admin',
     }
 }
 
